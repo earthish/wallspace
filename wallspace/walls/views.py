79752143938cbfd5da2e78c4a2266a758b3aaba0
earthish <prithvish04@gmail.com>
2026-06-01 16:47:20 +0530

    views.py gets its stuf

--- wallspace/walls/views.py
+++ wallspace/walls/views.py
@@ -5,4 +5,31 @@ from .models import Wall
 from .forms import WallForm
 # Create your views here.
 
-@login_required 
+@login_required #decorator, it checks automatically if a certain condition is satisfied or not
+def home(request):
+    if request.method=='POST':
+        form=WallForm(request.POST)
+
+        if form.is_valid():
+            wall = form.save(commit=False)
+            wall.owner= request.user
+            wall.save()
+
+            return redirect('home')
+    
+    else:
+        form = WallForm()
+
+    walls = Wall.objects.filter(
+        owner=request.user
+    )
+
+    context={
+        'walls':walls,
+        'form':form
+    }
+
+    return render(
+        request, 'walls/home.html', context
+
+    )
