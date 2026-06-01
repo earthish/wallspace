2808e8c9e4020356ef7ae2bb3f32960af56e66f6
earthish <prithvish04@gmail.com>
2026-06-01 15:24:02 +0530

    settings updated for dir info, urls.py for home,

--- wallspace/wallspace/settings.py
+++ wallspace/wallspace/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'wallspace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = 'static/'
 
 
