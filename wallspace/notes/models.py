009692b36a8dba2bcb16ac42764bf78f352a248e
earthish <prithvish04@gmail.com>
2026-06-01 14:37:55 +0530

    added walls model and notes model

--- wallspace/notes/models.py
+++ wallspace/notes/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+from walls.models import Wall
 
 # Create your models here.
+
+class Note(models.Model):
+    
+    COLOR_CHOICES = [#COLOR_CHOICES is a list of tuples
+        # formatted like this: (database_value, human_readable_label)
+        ('yellow','Yellow'),
+        ('pink', 'Pink'),
+        ('blue', 'Blue'),
+        ('green', 'Green'),
+    ]
+
+    wall = models.ForeignKey( #foreign key
+        Wall,
+        on_delete=models.CASCADE,
+        related_name='notes'
+    )
+
+    creator=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    content = models.TextField()
+
+    color= models.CharField(
+        max_length=20,
+        choices=COLOR_CHOICES,
+        default='yellow'
+    )
+    x_position = models.IntegerField(
+        default=50
+    )
+
+    y_position = models.IntegerField(
+        default=50
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f'{self.creator} note'
+
--- wallspace/walls/models.py
+++ wallspace/walls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Wall(models.Model):
+    title = models.CharField(max_length=100)
+    owner=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='walls'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.title #so it takes the title from the model while displaying a model
+    
