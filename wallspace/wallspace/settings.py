b58a56c538215b1b084dc1afe8d78ea66f593be9
earthish <prithvish04@gmail.com>
2026-06-01 11:57:22 +0530

    installed apps updated settings.py

--- wallspace/wallspace/settings.py
+++ wallspace/wallspace/settings.py
@@ -31,6 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'walls',
+    'notes',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
