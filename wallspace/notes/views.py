681415c912fc900df5bbda120be1d0c7bde1cfcd
earthish <prithvish04@gmail.com>
2026-06-03 15:56:08 +0530

    notes/views.py updated to return the jsonresponse

--- wallspace/notes/views.py
+++ wallspace/notes/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Note
+import json
 # Create your views here.
+
+def update_position(request):
+    if request.method=='POST':
+
+        data = json.loads(
+            request.body
+        )
+
+        note = Note.objects.get(
+            id=data['note_id']
+        )
+
+        note.x_position = data['x']
+        note.y_position = data['y']
+
+        note.save()
+        return JsonResponse({
+            'status': 'success'
+        })
+    return JsonResponse({
+        'status':'error'
+    })
